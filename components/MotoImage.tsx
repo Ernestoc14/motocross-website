cae5851a88f8092b03b5c2a47980157cbd9e104c
Ernestoc14 <descreed34@hotmail.com>
2024-07-04 14:55:24 -0500

    Fix: Background Image in white, one was missing the bg to match the others

--- components/MotoImage.tsx
+++ components/MotoImage.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import { getMotoImage } from "@/lib/actions/motos.action";
 
 export async function MotoImage({ articleID }: { articleID: number }) {
-  const image = await getMotoImage({articleID});
-  return <Image src={image.link} alt="Moto Image" width={248} height={248} />;
+  const image = await getMotoImage({ articleID });
+  return (
+    <div className="bg-white ">
+      <Image src={image.link} alt="Moto Image" width={248} height={248} />
+    </div>
+  );
 }
