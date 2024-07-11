0e1b4f606fd04f568a55a0b1f88062f26b495107
Ernestoc14 <descreed34@hotmail.com>
2024-07-11 14:12:49 -0500

    Feat: Updating Hero Component with Image and Styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   const handleScroll = () => {};
   return (
-    <div>
-      <div>
-        <h1>Find, book or rent a motorcycle -- quickly and easily!</h1>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book or rent a motorcycle -- quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your moto rental experience with our efforless booking
+          process.
+        </p>
+        {/* <CustomButton /> */}
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/wallpaper.jpg"
+            alt="hero"
+            fill
+            className="object-contain"
+          />
+        </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
